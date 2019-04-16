1181dab7378625326a00773a33e7ea1ee0a9ec4c
kale <intharaksak@gmail.com>
2019-04-16 17:05:55 -0400

    final

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,65 +1,99 @@
-$("#btnContainer").on("click", function (){
-    game.start();
-})
-$(document).on("click", "#button", function () {
-    game.done();
-})
+window.onload = function() {
+    $("#tipOff").on("click", stopwatch.start);
+    $(".choice").on("click", stopwatch.correctCount);
+    $("#doneBtn").on("click", stopwatch.stop);
+    $("restartBtn").on("click", stopwatch.reset);
+};
+var intervalId;
+var clockRunning = false;
+var rightAnswers = 0;
 
-var questions = [{
-    question: "What team does Lebron James play for?",  
-    answers: ["Cavs ", "Knicks ", "Magic ", "Lakers "],
-    correctAnswer: " Lakers "
-    }, {
-    question: "What is Joel Embiids Nickname?", 
-    answers: ["JoJo ", "The Process ", "Joey bad of Doughnuts ", "Joey "],
-    correctAnswer: " The Process "
-    }, {
-    question: "How many NBA teams are in Florida?",  
-    answers: ["1 ", "2 ", "3", "4 "], 
-    correctAnswer: " 2 "
-    }, {
-    question: "What team does Steph Curry play for?", 
-    answers: ["Magic", "Mavs ", "Warriors ", "Hornets "],
-    correctAnswer: " Warriros "
-    }, {
-    question: "What Team has home games in Canada?", 
-    answers: ["Spurs ", "Raptors", "Pacers ", "Heat "],
-    correctAnswer: " Raptors "
-    }, {
-    question: "What team drafted Carmelo Anthony?", 
-    answers: ["Lakers ", "Knicks", "Kings ", "Nuggets "],
-    correctAnswer: " Nuggets "
-    }, {
-    question: "How many teams play in California?", 
-    answers: ["1 ", "2 ", "3", "4 "],
-    correctAnswer: " 4"
-    }, {
-    question: "How many Championchips does Lebron James have?", 
-    answers: ["1", "3 ", "5 ", "6 "],
-    correctAnswer: " 3 "
-    }, {
-   
-    }
-    ];
-    var game = {
-        correct: 0,
-        incorrect: 0,
-        counter: 60,
-        countdown: function(){
-            game.counter--;
-            $("#counter").html(game.counter);
-            if (game.counter<=0){
-                console.log("Shot Clock Violation");
-                game.done();
+var stopwatch = {
+
+    
+    time: 60,
+
+    correctCount: function() {
+        
+        
+        if (clockRunning) {
+            var selection = $(this).val().trim();
+            if (selection === "correct" && rightAnswers < 8) {
+                rightAnswers++
+            }
+
+            
+            else if (rightAnswers > 7) {
+                stopwatch.stop();
             }
         }
-    }
-     start:function () {
-        timer = setInterval(play.countdown, 1000);
-        $("#subWrapper").prepend("<h2>Time Remaining: <span id='counter'>60</span> Seconds</h2>"); 
-        $("#startButton").remove();
-        for (var i=0; i < questions.length; i++) {
-        $("#subWrapper").append("<h2>"+questions[i].question+"</h2>");
-        for (var j=0; j < questions[i].answers.length; j++) {
-        $("#subWrapper").append("<input type='radio' name='question-"+i+"' value='+questions[i].answers[j]+'>"+questions[i].answers[j]);
+
+        
+        else if (!clockRunning) {
+            event.preventDefault();
         }
+    },
+
+    
+    reset: function() {
+
+        
+        stopwatch.stop();
+        stopwatch.time = 60;
+        rightAnswers = 0;
+        $("#timeLeft").text("1:00");
+        $("input[type='radio']").prop('checked', false);
+    },
+
+    
+    start: function() {
+        if (!clockRunning) {
+            intervalId = setInterval(stopwatch.count, 1000);
+            clockRunning = true;
+        }
+    },
+
+    
+    stop: function() {
+        clearInterval(intervalId);
+        clockRunning = false;
+
+        
+        $("#timeLeft").html("Score:" + rightAnswers + "/8");
+    },
+
+    
+    count: function() {
+
+        
+        if (stopwatch.time > 0) {
+            stopwatch.time--;
+            var converted = stopwatch.timeConverter(stopwatch.time);
+
+           
+            $("#timeLeft").text(converted);
+        }
+
+        
+        else {
+            stopwatch.stop();
+        }
+    },
+
+    
+    timeConverter: function(t) {
+        var minutes = Math.floor(t / 60);
+        var seconds = t - (minutes * 60);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (minutes === 0) {
+            minutes = "00";
+        }
+        else if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        return minutes + ":" + seconds;
+        }
+};                
+       
